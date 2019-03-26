6bad5e72244f418463130450ccfe1406bec3043d
pahacofome <none@none.no>
2019-03-26 12:52:19 +0800

    upgrade edit_handlers API to 2.0

--- wagtailmedia/edit_handlers.py
+++ wagtailmedia/edit_handlers.py
@@ -5,19 +5,8 @@ from wagtail.admin.edit_handlers import BaseChooserPanel
 from wagtailmedia.widgets import AdminMediaChooser
 
 
-class BaseMediaChooserPanel(BaseChooserPanel):
+class MediaChooserPanel(BaseChooserPanel):
     object_type_name = 'media'
 
     def widget_overrides(self):
         return {self.field_name: AdminMediaChooser}
-
-
-class MediaChooserPanel(object):
-    def __init__(self, field_name):
-        self.field_name = field_name
-
-    def bind_to_model(self, model):
-        return type(str('_MediaChooserPanel'), (BaseMediaChooserPanel,), {
-            'model': model,
-            'field_name': self.field_name,
-        })
