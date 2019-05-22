4ca544cc2773f8c1e292f8965d024338fe5692d2
Thibaud Colas <thibaudcolas@gmail.com>
2019-05-22 11:45:18 +0100

    Add deprecation path for Wagtail 2.0 edit_handlers API update

--- wagtailmedia/edit_handlers.py
+++ wagtailmedia/edit_handlers.py
@@ -5,6 +5,17 @@ from wagtail.admin.edit_handlers import BaseChooserPanel
 from wagtailmedia.widgets import AdminMediaChooser
 
 
+class BaseMediaChooserPanel(BaseChooserPanel):
+    """
+    DEPRECATED – This undocumented API will be removed in a future release. Please use MediaChooserPanel instead.
+    Note: When removing this deprecated code, just remove the whole class.
+    """
+    object_type_name = 'media'
+
+    def widget_overrides(self):
+        return {self.field_name: AdminMediaChooser}
+
+
 class MediaChooserPanel(BaseChooserPanel):
     object_type_name = 'media'
 
