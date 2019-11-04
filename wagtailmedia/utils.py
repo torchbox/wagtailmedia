800842421d8c7397d385ce727b01d2cdec65cb71
Chris May <chmay@markelcorp.com>
2019-11-04 11:24:37 -0500

    Fixing flake8 error

--- wagtailmedia/utils.py
+++ wagtailmedia/utils.py
@@ -7,7 +7,6 @@ else:
 
     DEFAULT_PAGE_KEY = 'p'
 
-
     def paginate(request, items, page_key=DEFAULT_PAGE_KEY, per_page=20):
         paginator = Paginator(items, per_page)
         page = paginator.get_page(request.GET.get(page_key))
