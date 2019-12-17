02360ab4282fa418ad8bb4dcd1c9a91c3486e9f9
Gordon Pendleton <wgordonw1@gmail.com>
2019-12-17 16:27:12 -0500

    use url instead of re_path for dj111 compat

--- wagtailmedia/tests/urls.py
+++ wagtailmedia/tests/urls.py
@@ -1,7 +1,7 @@
 import re
 
 from django.conf import settings
-from django.conf.urls import include, re_path, url
+from django.conf.urls import include, url
 from django.views.static import serve
 
 from wagtail.admin import urls as wagtailadmin_urls
@@ -11,7 +11,7 @@ urlpatterns = [
     url(r'^admin/', include(wagtailadmin_urls)),
     url(r'', include(wagtail_urls)),
 ] + [
-    re_path(r'^%s(?P<path>.*)$' % re.escape(prefix.lstrip('/')), serve, kwargs={'document_root': document_root})
+    url(r'^%s(?P<path>.*)$' % re.escape(prefix.lstrip('/')), serve, kwargs={'document_root': document_root})
     for prefix, document_root in (
         (settings.STATIC_URL, settings.STATIC_ROOT),
         (settings.MEDIA_URL, settings.MEDIA_ROOT),
