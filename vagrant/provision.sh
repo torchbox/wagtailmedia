43ade5e9fc5f2acf7d7a8804d94068723df8b08e
Thibaud Colas <thibaudcolas@gmail.com>
2020-11-06 12:03:00 +0000

    Remove support for Python 3.5

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -8,7 +8,7 @@ export DEBIAN_FRONTEND=noninteractive
 
 add-apt-repository -y ppa:deadsnakes/ppa
 apt-get update
-apt-get install -y python3-distutils build-essential libssl-dev python-dev python3.5 python3.5-dev python3.6 python3.6-dev python3.7 python3.7-dev
+apt-get install -y python3-distutils build-essential libssl-dev python-dev python3.6 python3.6-dev python3.7 python3.7-dev
 
 curl -sSL https://get.docker.com/ | sh
 adduser vagrant docker
