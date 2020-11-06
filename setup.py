6cd29f87c825f8c9c2c163d04d8896dd3611bc10
Thibaud Colas <thibaudcolas@gmail.com>
2020-11-06 13:35:17 +0000

    Update list of Django / Wagtail versions tested in CI

--- setup.py
+++ setup.py
@@ -61,6 +61,7 @@ setup(
         'Framework :: Django :: 2.1',
         'Framework :: Django :: 2.2',
         'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
         'Framework :: Wagtail :: 2',
         'Topic :: Internet :: WWW/HTTP :: Site Management',
     ],
