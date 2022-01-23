torchbox/wagtailmedia

commit 5e2f95eb6a8e5700b771e9370a28e8f9c21bf145

Update tox config

tox.ini
@@ -1,81 +1,54 @@
 [tox]
-requires = tox-docker
-skipsdist = True
-toxworkdir = /home/vagrant/.tox
 usedevelop = True
 
 envlist =
-    py{36,37,38,39}-dj{22,31}-wag{211,212,213}
-    py{38,39}-dj{32}-wag{213}
+    py{36,37,38,39}-dj{22,31}-wagtail{211,212,213,214}
+    py{38,39}-dj{32}-wagtail{213,214}
     interactive
-    flake
-    isort
-
 
 [testenv]
-install_command = pip install -e ".[testing]" -U {opts} {packages}
+setenv =
+    PYTHONPATH = {toxinidir}
+    DJANGO_SETTINGS_MODULE = tests.settings
 
-commands =
-    {posargs:coverage run runtests.py}
-
-basepython =
-    py36: python3.6
-    py37: python3.7
-    py38: python3.8
-    py39: python3.9
-    {interactive,flake,isort}: python3.9
+extras = testing
 
 deps =
     dj22: Django>=2.2,<2.3
     dj31: Django>=3.1,<3.2
-    dj32: Django>=3.2,<3.3
-    wag211: wagtail>=2.11,<2.12
-    wag212: wagtail>=2.12,<2.13
-    wag213: wagtail>=2.13,<2.14
-    {interactive,flake,isort}: wagtail
+    dj32: Django>=3.1,<3.2
+    wagtail211: wagtail>=2.11,<2.12
+    wagtail212: wagtail>=2.12,<2.13
+    wagtail213: wagtail>=2.13,<2.14
+    wagtail214: wagtail>2.13,<2.15
+    interactive: wagtail>2.13,<2.15
 
+install_command = pip install -U {opts} {packages}
 
-[testenv:flake]
-commands = {posargs:flake8 wagtailmedia}
+commands_pre =
+    python {toxinidir}/manage.py migrate
 
-
-[testenv:isort]
-commands = {posargs:isort --check-only --diff wagtailmedia}
-
+commands =
+    coverage run runtests.py {posargs: -v 2}
+    coverage report -m
 
 [testenv:interactive]
+basepython = python3.9
+
 commands_pre =
     python {toxinidir}/manage.py makemigrations
     python {toxinidir}/manage.py migrate
-    python {toxinidir}/manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('super', 'super@example.com', 'super')"
+    python {toxinidir}/manage.py shell -c "from django.contrib.auth.models import User;(not User.objects.filter(username='admin').exists()) and User.objects.create_superuser('admin', 'super@example.com', 'changeme')"
+    python {toxinidir}/manage.py createcachetable
 
 commands =
     {posargs:python manage.py runserver 0.0.0.0:8020}
 
-docker =
-    postgres:12.0
-    redis:5.0
-
-dockerenv =
-    POSTGRES_PASSWORD=pgpass
-    POSTGRES_USER=pguser
-    POSTGRES_DB=pgdb
-
 setenv =
     INTERACTIVE = 1
 
-
-[docker:redis:5.0]
-healthcheck_cmd = redis-cli ping | grep -q PONG
-healthcheck_interval = 3
-healthcheck_timeout = 3
-healthcheck_retries = 30
-healthcheck_start_period = 5
-
-
-[docker:postgres:12.0]
-healthcheck_cmd = pg_isready
-healthcheck_interval = 3
-healthcheck_timeout = 3
-healthcheck_retries = 30
-healthcheck_start_period = 5
+[testenv:wagtailmain]
+usedevelop = true
+deps =
+    coverage
+    wagtailmain: git+https://github.com/wagtail/wagtail.git@main#egg=Wagtail

.coveragerc
@@ -6,7 +6,7 @@ source = wagtailmedia
 
 omit =
     */migrations/*
-    */tests/*
+    tests/*
 
 
 [report]

setup.cfg
@@ -13,7 +13,7 @@ extend-ignore = E203
 known_first_party=wagtailmedia
 known_django=django
 known_wagtail=wagtail,modelcluster
-skip=migrations,.git,__pycache__,LC_MESSAGES,locale,build,dist,.github
+skip=migrations,.git,__pycache__,LC_MESSAGES,locale,build,dist,.github,.tox,venv
 blocked_extensions=rst,html,js,svg,txt,css,scss,png,snap,tsx,sh
 sections=FUTURE,STDLIB,DJANGO,WAGTAIL,THIRDPARTY,FIRSTPARTY,LOCALFOLDER
 default_section=THIRDPARTY

setup.py
@@ -17,13 +17,7 @@ testing_extras = [
     "mock>=1.0.0",
     # For coverage and PEP8 linting
     "coverage>=3.7.0",
-    "flake8>=3.6.0",
-    "isort==5.6.4",  # leave this pinned - it tends to change rules between patch releases
-    # Required for running tests in Vagrant
-    "tox==3.9.0",
-    # Required for interactive testing via tox
-    "psycopg2-binary<2.9",
-    "django-redis-cache",
+    "tox==3.23.1",
 ]
 
 with io.open("README.md", encoding="utf-8") as readme_file:
@@ -64,7 +58,7 @@ setup(
         "Topic :: Internet :: WWW/HTTP :: Site Management",
     ],
     install_requires=[
-        "wagtail>=2.7",
+        "wagtail>=2.11",
     ],
     extras_require={
         "testing": testing_extras,

commit f451643ecb2a27d0f459a9147f7cb0606e5eb563

Remov module level call to ``get_media_base_form()`` (#148)
This caused import issues for projects that subclassed ``BaseMediaForm``.
Updated tests to rely on overriding settings instead of mocking the return value.
Fixed tox config for dj32, which was accidentally only testing Django 3.1.x (the bug was introduced in 5e2f95eb6a).

Fixes #147

src/wagtailmedia/forms.py
@@ -45,9 +45,6 @@ def get_media_base_form():
     return base_form
 
 
-media_base_form = get_media_base_form()
-
-
 def get_media_form(model):
     fields = model.admin_form_fields
     if "collection" not in fields:
@@ -59,7 +56,7 @@ def get_media_form(model):
 
     return modelform_factory(
         model,
-        form=media_base_form,
+        form=get_media_base_form(),
         fields=fields,
     )
 

tox.ini
@@ -19,7 +19,7 @@ deps =
     coverage
     dj22: Django>=2.2,<2.3
     dj31: Django>=3.1,<3.2
-    dj32: Django>=3.1,<3.2
+    dj32: Django>=3.2,<3.3
     wagtail211: wagtail>=2.11,<2.12
     wagtail214: wagtail>=2.14,<2.15
     wagtail215: wagtail>=2.15,<2.16

tests/test_form_override.py
@@ -1,5 +1,3 @@
-from unittest.mock import patch
-
 from django import forms
 from django.test import TestCase, override_settings
 
@@ -7,18 +5,10 @@ from wagtail.admin import widgets
 
 from tests.testapp.forms import AlternateMediaForm, OverridenWidget
 from wagtailmedia import models
-from wagtailmedia.forms import (
-    BaseMediaForm,
-    get_media_base_form,
-    get_media_form,
-    media_base_form,
-)
+from wagtailmedia.forms import BaseMediaForm, get_media_base_form, get_media_form
 
 
 class TestFormOverride(TestCase):
-    def test_media_base_form(self):
-        self.assertIs(media_base_form, BaseMediaForm)
-
     def test_get_media_base_form(self):
         self.assertIs(get_media_base_form(), BaseMediaForm)
 
@@ -40,13 +30,17 @@ class TestFormOverride(TestCase):
     def test_overridden_base_form(self):
         self.assertIs(get_media_base_form(), AlternateMediaForm)
 
-    @patch("wagtailmedia.forms.media_base_form", AlternateMediaForm)
+    @override_settings(
+        WAGTAILMEDIA={"MEDIA_FORM_BASE": "tests.testapp.forms.AlternateMediaForm"}
+    )
     def test_get_overridden_media_form(self):
         bases = get_media_form(models.Media).__bases__
         self.assertNotIn(BaseMediaForm, bases)
         self.assertIn(AlternateMediaForm, bases)
 
-    @patch("wagtailmedia.forms.media_base_form", AlternateMediaForm)
+    @override_settings(
+        WAGTAILMEDIA={"MEDIA_FORM_BASE": "tests.testapp.forms.AlternateMediaForm"}
+    )
     def test_get_overridden_media_form_widgets(self):
         Form = get_media_form(models.Media)
         form = Form()

tests/testapp/forms.py
@@ -1,12 +1,13 @@
-from django.forms import ModelForm
 from django.forms.widgets import Widget
+
+from wagtailmedia.forms import BaseMediaForm
 
 
 class OverridenWidget(Widget):
     pass
 
 
-class AlternateMediaForm(ModelForm):
+class AlternateMediaForm(BaseMediaForm):
     class Meta:
         widgets = {
             "tags": OverridenWidget,