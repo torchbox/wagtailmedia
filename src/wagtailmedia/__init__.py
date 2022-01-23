0562deac210710a444fcb5808ab84d656bee70f3
Dan Braghis <dan.braghis@torchbox.com>
2022-01-23 12:27:16 +0100

    Fix deprecations

--- src/wagtailmedia/__init__.py
+++ src/wagtailmedia/__init__.py
@@ -1,4 +1,10 @@
+from django import VERSION as DJANGO_VERSION
+
 from .version import __version__  # noqa
 
 
-default_app_config = "wagtailmedia.apps.WagtailMediaAppConfig"
+if DJANGO_VERSION >= (3, 2):
+    # The declaration is only needed for older Django versions
+    pass
+else:
+    default_app_config = "wagtailmedia.apps.WagtailMediaAppConfig"
