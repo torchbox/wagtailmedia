ee4b809ffe374e7a6d58c43d5254647413b517e6
zerolab <dan@zerolab.org>
2023-02-15 18:07:01 +0000

    Lint

--- src/wagtailmedia/migrations/0004_duration_optional_floatfield.py
+++ src/wagtailmedia/migrations/0004_duration_optional_floatfield.py
@@ -5,7 +5,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("wagtailmedia", "0003_copy_media_permissions_to_collections"),
     ]
--- tests/testapp/migrations/0001_initial.py
+++ tests/testapp/migrations/0001_initial.py
@@ -15,7 +15,6 @@ import wagtailmedia.blocks
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
     dependencies = [
