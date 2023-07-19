cca59069290e40e2df0a9a2e9126e7fe49b3e567
zerolab <dan@zerolab.org>
2023-07-19 12:00:45 +0100

    Fix widget initialisation post chooser buttons redesign
    See https://github.com/wagtail/wagtail/pull/10557

--- src/wagtailmedia/static/wagtailmedia/js/media-chooser.js
+++ src/wagtailmedia/static/wagtailmedia/js/media-chooser.js
@@ -2,26 +2,38 @@ function createMediaChooser(id) {
     const chooserElement = $('#' + id + '-chooser');
     const mediaTitle = chooserElement.find('[data-chooser-title]');
     const input = $('#' + id);
-    const editLink = chooserElement.find('.edit-link');
-    const chooserBaseUrl = chooserElement.data('chooserUrl');
 
-    $('.action-choose', chooserElement).on('click', function() {
+    let chooseAction = chooserElement.find('.action-choose');
+    if (!chooseAction.length) {
+        chooseAction = chooserElement.find('[data-chooser-action-choose]');
+    }
+    let clearAction = chooserElement.find('.action-clear');
+    if (!clearAction.length) {
+        clearAction = chooserElement.find('[data-chooser-action-clear]');
+    }
+    let editAction = chooserElement.find('.edit-link');
+    if (!editAction.length) {
+        editAction = chooserElement.find('[data-chooser-edit-link]');
+    }
+
+
+    chooseAction.on('click', function() {
         ModalWorkflow({
-            url: chooserBaseUrl,
+            url: chooserElement.data('chooserUrl'),
             onload: MEDIA_CHOOSER_MODAL_ONLOAD_HANDLERS,
             responses: {
                 mediaChosen: function(mediaData) {
                     input.val(mediaData.id);
                     mediaTitle.text(mediaData.title);
                     chooserElement.removeClass('blank');
-                    editLink.attr('href', mediaData.edit_link);
-                    editLink.removeClass('w-hidden');
+                    editAction.attr('href', mediaData.edit_url);
+                    editAction.removeClass('w-hidden');
                 }
             }
         });
     });
 
-    $('.action-clear', chooserElement).on('click', function() {
+    clearAction.on('click', function() {
         input.val('');
         chooserElement.addClass('blank');
     });
@@ -42,8 +54,8 @@ function createMediaChooser(id) {
             }
             input.val(mediaData.id);
             mediaTitle.text(mediaData.title);
-            editLink.attr('href', mediaData.edit_url);
-            editLink.removeClass('w-hidden');
+            editAction.attr('href', mediaData.edit_url);
+            editAction.removeClass('w-hidden');
             chooserElement.removeClass('blank');
             state = mediaData;
         },
@@ -57,7 +69,7 @@ function createMediaChooser(id) {
             return result;
         },
         focus: function() {
-            $('.action-choose', chooserElement).focus();
+            chooseAction.focus();
         }
     };
 
