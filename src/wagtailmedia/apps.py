c822f70b7c01d703a9353f308c25cab85c347694
zerolab <dan@zerolab.org>
2026-08-21 16:56:04 +0100

    Fix tests

--- src/wagtailmedia/apps.py
+++ src/wagtailmedia/apps.py
@@ -2,6 +2,8 @@ from django.apps import AppConfig
 from django.db.models import ForeignKey
 from wagtail import VERSION as WAGTAIL_VERSION
 
+from . import get_media_model, get_permission_policy
+
 
 class WagtailMediaAppConfig(AppConfig):
     default_auto_field = "django.db.models.AutoField"
@@ -12,21 +14,19 @@ class WagtailMediaAppConfig(AppConfig):
     def ready(self):
         from wagtail.admin.compare import register_comparison_class
 
-        from . import get_media_model
         from .edit_handlers import MediaFieldComparison
         from .signal_handlers import register_signal_handlers
+
+        Media = get_media_model()
+        if WAGTAIL_VERSION >= (8, 0):
+            from wagtail.permissions import register_permission_policy
+
+            register_permission_policy(Media, get_permission_policy())
 
         register_signal_handlers()
 
         # Set up image ForeignKeys to use MediaFieldComparison as the comparison class
         # when comparing page revisions
         register_comparison_class(
-            ForeignKey, to=get_media_model(), comparison_class=MediaFieldComparison
+            ForeignKey, to=Media, comparison_class=MediaFieldComparison
         )
-
-        if WAGTAIL_VERSION >= (8, 0):
-            from wagtail.permissions import register_permission_policy
-
-            from .permissions import get_permission_policy
-
-            register_permission_policy(get_media_model(), get_permission_policy())
