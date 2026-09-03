67459e7d89be4832f11be06a3be93964dfbecf64
Dan Braghiș <31622+zerolab@users.noreply.github.com>
2026-09-03 14:53:59 +0100

    [Wagtail 8.0] Add support for custom base page models (#283)
    * Add Wagtail 8 to the test matrix
    * Handle custom base pages
       Docs: https://docs.wagtail.org/en/latest/advanced_topics/customization/custom_base_page_models.html
       Example project: https://github.com/wagtail/wagtail-custom-base-page-template
    * Fixes and lean up

--- tests/testapp/settings.py
+++ tests/testapp/settings.py
@@ -1,5 +1,7 @@
 import os
 
+from wagtail import VERSION as WAGTAIL_VERSION
+
 
 DEBUG = "INTERACTIVE" in os.environ
 
@@ -109,3 +111,15 @@ ALLOWED_HOSTS = ["*"]
 
 WAGTAIL_SITE_NAME = "wagtailmedia Test Site"
 WAGTAILADMIN_BASE_URL = "http://localhost:8020"
+
+USE_EXTENDS = False
+
+if WAGTAIL_VERSION >= (8, 0):
+    if os.environ.get("USES_CUSTOM_PAGE_MODEL"):
+        INSTALLED_APPS.insert(2, "testapp.basepage")
+        WAGTAIL_PAGE_MODEL = "basepage.BasePage"
+        print("Custom base page model active [wagtailmedia]")
+
+    if os.environ.get("USE_EXTENDS"):
+        INSTALLED_APPS.insert(0, "testextends")
+        USE_EXTENDS = True
